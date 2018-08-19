6b9ab70d0eb083b9e23aa6ade27bc0d6942950f0
Soanker Srinivasa <saonker@gmail.com>
2018-08-19 21:16:30 +0530

    second commit

--- app/src/main/java/in/izba/kotlintest/MainActivity.kt
+++ app/src/main/java/in/izba/kotlintest/MainActivity.kt
@@ -1,11 +1,12 @@
 package `in`.izba.kotlintest
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        // first kotlin programme for testing
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
