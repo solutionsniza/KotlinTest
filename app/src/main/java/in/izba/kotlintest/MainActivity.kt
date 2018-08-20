914c04576c6ebce33f19888a14bcf077409d652d
Soanker Srinivasa <saonker@gmail.com>
2018-08-20 23:54:14 +0530

    added constraint layout for landscape and potrait

--- app/src/main/java/in/izba/kotlintest/MainActivity.kt
+++ app/src/main/java/in/izba/kotlintest/MainActivity.kt
@@ -2,6 +2,10 @@ package `in`.izba.kotlintest
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +13,21 @@ class MainActivity : AppCompatActivity() {
         // first kotlin programme for testing
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.helloClick) as Button
+        button.setOnClickListener{
+            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+        }
+
+        val text: TextView = findViewById<TextView>(R.id.headerText) as TextView
+        text.setOnClickListener {
+            text.setText(getString(R.string.name))
+        }
+
+        val imageView: ImageView = findViewById<ImageView>(R.id.myImage) as ImageView
+        imageView.setOnClickListener{
+            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
